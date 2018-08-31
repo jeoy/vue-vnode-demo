d26711812214a2a1c78e66123c52a3b18607b3d5
zhongyi <zhongyi@gridsum.com>
2018-08-31 15:47:53 +0800

    fix: init

---
+++ childrenComp.render.js
@@ -0,0 +1,59 @@
+(function anonymous() {
+    with(this) {
+        return _c(
+            'div', {
+                attrs: {
+                    "id": "app"
+                }
+            }, [_c('div', _l((arr), function(item, index) {
+                return _c("comp-child", {
+                    key: index,
+                    tag: "component",
+                    attrs: {
+                        "parent-data": item
+                    }
+                }, [_c('button', {
+                    on: {
+                        "click": function($event) {
+                            deleteItem(index)
+                        }
+                    }
+                }, [_v("delete")])])
+            }))])
+    }
+})
+
+
+
+ƒ(item, index) {
+    return _c("comp-child", {
+        key: index + 5,
+        tag: "component",
+        attrs: {
+            "parent-data": item
+        }
+    }, [_c('button', {
+        on: {
+            "click": function($event) {
+                deleteItem(index)
+            }
+        }
+    }, [_v("delete")])])
+}
+
+
+ƒ(item, index) {
+    return _c("comp-child", {
+        key: item.name,
+        tag: "component",
+        attrs: {
+            "parent-data": item
+        }
+    }, [_c('button', {
+        on: {
+            "click": function($event) {
+                deleteItem(index)
+            }
+        }
+    }, [_v("delete")])])
+}
